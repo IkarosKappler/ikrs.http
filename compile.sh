6887ad5cc500499264d443fe67d7db88cccf25de
Ikaros Kappler <ikaros.kappler@gmx.de>
2013-01-04 01:29:15 +0100

    ExecutionRejected_handler

--- compile.sh
+++ compile.sh
@@ -1,5 +1,69 @@
 #!/bin/sh
+#
+# @author Ikaros Kappler
+# @date 2012-05-01
+# @modified 2012-12-19
+# @version 1.0.0
 
-javac -Xlint:unchecked ikrs/yuccasrv/*.java ikrs/yuccasrv/ui/*.java ikrs/util/*.java ikrs/yuccasrv/commandline/*.java ikrs/yuccasrv/socketmngr/*.java ikrs/http/*.java ikrs/http/response/successful/*.java ikrs/http/response/*.java ikrs/http/resource/*.java ikrs/util/session/*.java ikrs/typesystem/*.java ikrs/io/fileio/htaccess/*.java ikrs/io/fileio/inifile/*.java ikrs/http/filehandler/*.java ikrs/io/*.java ikrs/http/datatype/*.java
 
-# ikrs/http/response/servererror/*.java ikrs/http/response/redirection/*.java ikrs/http/response/clienterror/*.java
+# Check file argument?
+# if [ "$1" = "" ]; then
+#    echo "Please pass the input file"
+#    exit 1
+# fi
+
+
+# Compile the utils
+echo "Compile ikrs.util ..."
+javac -Xlint:unchecked ikrs/util/*.java ikrs/util/session/*.java 
+ec="$?"
+if [ "$ec" -ne "0" ]; then
+    echo "Sorry, failed to compile ikrs.util. Exit code $ec"
+    exit 1
+fi
+
+
+echo "Compile ikrs.typesystem ..."
+javac -Xlint:unchecked ikrs/typesystem/*.java
+ec="$?"
+if [ "$ec" -ne "0" ]; then
+    echo "Sorry, failed to compile ikrs.typesystem. Exit code $ec"
+    exit 1
+fi
+
+
+echo "Compile ikrs.io ..."
+javac -Xlint:unchecked ikrs/io/*.java ikrs/io/fileio/*.java ikrs/io/fileio/htaccess/*.java ikrs/io/fileio/inifile/*.java
+ec="$?"
+if [ "$ec" -ne "0" ]; then
+    echo "Sorry, failed to compile ikrs.io. Exit code $ec"
+    exit 1
+fi
+
+
+echo "Compile ikrs.yucca ..."
+javac -Xlint:unchecked ikrs/yuccasrv/*.java ikrs/yuccasrv/ui/*.java ikrs/yuccasrv/commandline/*.java ikrs/yuccasrv/socketmngr/*.java
+ec="$?"
+if [ "$ec" -ne "0" ]; then
+    echo "Sorry, failed to compile ikrs.yucca. Exit code $ec"
+    exit 1
+fi
+
+
+echo "Compile ikrs.httpd ..."
+javac -Xlint:unchecked ikrs/httpd/*.java ikrs/httpd/response/successful/*.java ikrs/httpd/response/*.java ikrs/httpd/resource/*.java ikrs/httpd/filehandler/*.java ikrs/httpd/datatype/*.java
+ec="$?"
+if [ "$ec" -ne "0" ]; then
+    echo "Sorry, failed to compile ikrs.httpd. Exit code $ec"
+    exit 1
+fi
+
+
+echo "Done."
+
+
+# This is the old compiler command: compiling *all* in a bunch
+# javac -Xlint:unchecked ikrs/yuccasrv/*.java ikrs/yuccasrv/ui/*.java ikrs/util/*.java ikrs/yuccasrv/commandline/*.java ikrs/yuccasrv/socketmngr/*.java ikrs/httpd/*.java ikrs/httpd/response/successful/*.java ikrs/httpd/response/*.java ikrs/httpd/resource/*.java ikrs/util/session/*.java ikrs/typesystem/*.java ikrs/io/fileio/htaccess/*.java ikrs/io/fileio/inifile/*.java ikrs/httpd/filehandler/*.java ikrs/io/*.java ikrs/io/fileio/*.java ikrs/httpd/datatype/*.java
+
+
+
--- yucca.sh
+++ yucca.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-java ikrs.yuccasrv.Yucca --config _.yuccasrv/server.xml -c -l FINEST
+# This would be the testing mode with existing (!) configuration directories.
+java ikrs.yuccasrv.Yucca --config {USER_HOME}/.yuccasrv/server.xml -c -l FINEST
+
+# This is the real command to run ikrs.yucca/ikrs.httpd
+# java ikrs.yuccasrv.Yucca --config {USER_HOME}/.yuccasrv/server.xml -c -l FINEST
+
